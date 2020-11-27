73df3652395bf4b795fe64e73b594ed159f11bc9
priya-abc <priyapriya6598@gmail.com>
2020-11-27 15:59:27 +0530

    Added: Welcome message

---
+++ snake.java
@@ -0,0 +1,16 @@
+package sanke;
+
+import java.util.*;
+import java.util.Random;
+public class snake {
+	int startgame=0;
+    int endgame=0;
+    static int player1=0;
+    static int player2=0;
+    static int currentPlayer=-1;
+    static int position=0;
+    
+    public static void main (String [] args)
+    {
+    System.out.println("Welcome to snake ladder game");
+    
