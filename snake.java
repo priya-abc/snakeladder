c4139a2a77c5fb6986d3a2d29d00415479c5030a
priya-abc <priyapriya6598@gmail.com>
2020-11-27 16:10:38 +0530

    Added: case of snake and ladder added

--- snake.java
+++ snake.java
@@ -18,3 +18,27 @@ Random random = new Random();
     	int diceRoll1 = random.nextInt(6+1);
     	System.out.println("num is:" + diceRoll1);
     	
+int a=random.nextInt(3);
+    	System.out.println("case is:" +a);
+    	switch(a) {
+    	case 0:
+    		
+        	    position=position+diceRoll1;
+        		System.out.println("position is:" +position);
+        	    System.out.println("in case of ladder the player moves ahead by no of position");
+        break;
+    	
+    	case 1:
+    		position=position-diceRoll1;
+    		System.out.println("position is:" +position);
+    		System.out.println("in case of snake the player moves behind by no of position");
+    	break;
+    	    	
+    	
+    	    		
+    	default:
+    		
+    		System.out.println("no operation");
+    		break;
+    		
+    	
