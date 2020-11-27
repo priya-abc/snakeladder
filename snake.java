7b3540e95f070896faef6694934e3e6e1f01099d
priya-abc <priyapriya6598@gmail.com>
2020-11-27 16:38:42 +0530

    Added: playerswinnings added

--- snake.java
+++ snake.java
@@ -44,24 +44,28 @@ public class snake {
     		System.out.println("no operation");
     		break;
     		}
-            	
-   public void player1()
-   {
     		if(currentPlayer == -1)
     		{
     			
     		 	player1 = calculatePlayerValue(player1, diceRoll1);
     			System.out.println("First Player :: " + player1);
     			System.out.println("Second Player :: " + player2);
-    		}else{
-		
-		
-		
+                        }if(isWin(player1))
+		         {
+			System.out.println("First player wins");
+
+          		}
+    	            	}else{
 		player2 = calculatePlayerValue(player2,diceRoll1);
 		System.out.println("First Player :: " + player1);
 		System.out.println("Second Player :: " + player2);
 		System.out.println("------------------");
-
+               if(isWin(player2))
+		{
+			System.out.println("Second player wins");
+			return;
+		}
+	}
           }
       }
 }
