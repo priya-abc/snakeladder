9f547a925770c2575988e85a34ffdf3be968cdcc
priya-abc <priyapriya6598@gmail.com>
2020-11-27 21:09:38 +0530

    Added: playerswinnings added

--- snake.java
+++ snake.java
@@ -1,22 +1,30 @@
-package sanke;
+package snake;
 
 import java.util.*;
 import java.util.Random;
 public class snake {
-	int startgame=0;
-    int endgame=0;
+    static int startgame=0;
+    static int endgame=0;
     static int player1=0;
     static int player2=0;
-    static int currentPlayer=-1;
+    static int count1,count2,endgame1,endgame2;
     static int position=0;
-    
-    public static void main (String [] args)
-    {
+    public static void snakeLadder() {
     System.out.println("Welcome to snake ladder game");
     
-        Random random = new Random();
+    position=startgame;
+	endgame1=startgame;
+	endgame2=startgame;
+	count1=startgame;
+	count2=startgame;}
+    public void dice() {
+    	Random random = new Random();
     	int diceRoll1 = random.nextInt(6+1);
     	System.out.println("num is:" + diceRoll1);
+    	int diceRoll2 = random.nextInt(6+1);
+    	System.out.println("num is:" + diceRoll2);}
+    private void a(){
+    
         
         
         while(position<100) {
@@ -40,33 +48,55 @@ public class snake {
                System.out.println("Reached 100 Successfully");
         break;
     	default:
-    		
-    		System.out.println("no operation");
+System.out.println("no operation");
     		break;
-    		}
-    		if(currentPlayer == -1)
-    		{
-    			
-    		 	player1 = calculatePlayerValue(player1, diceRoll1);
-    			System.out.println("First Player :: " + player1);
-    			System.out.println("Second Player :: " + player2);
-                        }if(isWin(player1))
-		         {
-			System.out.println("First player wins");
+    		
+    	}
+    	
+    	}
+    }
 
-          		}
-    	            	}else{
-		player2 = calculatePlayerValue(player2,diceRoll1);
-		System.out.println("First Player :: " + player1);
-		System.out.println("Second Player :: " + player2);
-		System.out.println("------------------");
-               if(isWin(player2))
+    
+    public static void player1(Object roll)
+	{
+		roll=diceRoll1;
+		count1++;
+		endgame1=endgame2+position;
+		if(endgame1>100)
 		{
-			System.out.println("Second player wins");
-			return;
+			endgame1=endgame1-diceRoll1;
 		}
+		System.out.println("Player 1 New Position: "+endgame1);
 	}
-          }
-      }
+
+    public static void player2(int roll2)
+	{
+		roll2=diceRoll2;
+		count2++;
+		endgame2=endgame2+position;
+		if(endgame2>100)
+		{
+			endgame2=endgame2-diceRoll2;
+		}
+		System.out.println("Player 2 New Position: "+endgame2);
+	}
+        public static void main(String[] args) {
+	snakeLadder();
+	System.out.println("Initial Position: "+startgame);
+	while(endgame1 < endgame && endgame2 < endgame)
+	{
+	player1(roll);
+	player2(diceRoll2);
+	}
+	if(endgame1==100) {
+	System.out.println("Player1 has won: ");
+	System.out.println("The number of times the dice was rolled " +count1 +" times");
+	}
+	else if(endgame2==100) {
+		System.out.println("Player2 has won: ");
+		System.out.println("The number of times the dice was rolled " +count2 +" times");
+	}
+	else
+		System.out.println("Bad luck");
 }
-    	    	
+
